78aab972fc882c17b9de1d8b305de42f630e8589
kunalkeshan <kunalkeshan12@gmail.com>
2022-11-20 19:42:58 +0530

    chore: run shell script updated

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #! /usr/bin/sh
 
-sh install.sh
+read -p "Insall packages \"y\" for yes and \"n\" for no- " run_install
+
+if [[ "$run_install" == "y" ]]; then
+    sh install.sh
+fi
+
 read -p "Enter ngrok authtoken - " authtoken
+
 
 if [[ -z "$authtoken" ]]; then
     echo "Cannot boot application without ngrok authtoken, try again"
