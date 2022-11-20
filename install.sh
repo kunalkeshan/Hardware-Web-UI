cb6c33d43b627a066e729ea6e37fce550fbfaea5
kunalkeshan <kunalkeshan12@gmail.com>
2022-11-20 19:07:17 +0530

    chore: install shell script updated

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #! /usr/bin/sh
-echo "prod or dev"
-read env
-if ["$env" = "prod"]; then
+
+read -p "Install dependencies for, 'prod' or 'dev' - " env
+
+if [[ "$env" == "prod" ]]; then
+    echo "Installing dependencies from production.txt..."
     pip3 install -r production.txt
 else
+    echo "Installing dependencies from requirements.txt..."
     pip3 install -r requirements.txt
 fi
 
-# curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+if ! command -v ngrok &> /dev/null
+then
+    echo "ngrok could not be found, installing ngrok..."
+    curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+    exit
+fi
