46fa73aecd242935a2dd9955eec775ce40f479e1
kunalkeshan <kunalkeshan12@gmail.com>
2022-11-20 19:12:59 +0530

    chore: shell scripts updated

--- install.sh
+++ install.sh
@@ -14,5 +14,4 @@ if ! command -v ngrok &> /dev/null
 then
     echo "ngrok could not be found, installing ngrok..."
     curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
-    exit
 fi
--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #! /usr/bin/sh
-python main.py
-echo authtoken
-ngrok http 8000 $authtoken 
+
+sh install.sh
+read -p "Enter ngrok authtoken - " authtoken
+
+if [[ -z "$authtoken" ]]; then
+    echo "Cannot boot application without ngrok authtoken, try again"
+    exit
+else
+    python main.py
+    ngrok http 8000 $authtoken 
+fi
